63348b20c35cc84ff7027046ae6f0a577f489896
Jeswin <jeswinpk@agilehead.com>
2020-06-04 19:56:12 +0530

    More tests for onConnect

--- src/test/integration/connections/websocket/backends/http/onConnect.ts
+++ src/test/integration/connections/websocket/backends/http/onConnect.ts
@@ -10,6 +10,108 @@ export default async function (app: {
     websocketServers: WebSocket.Server[];
   };
 }) {
+  it(`runs the connect hook on root config`, async () => {
+    let ran = false;
+    let receivedMessage = "";
+    let clientResponse = "";
+
+    const connectedPromise: Promise<void> = new Promise(async (success) => {
+      const config = {
+        instanceId: random(),
+        webSocket: {
+          onConnect: async (requestId: string, message: string) => {
+            ran = true;
+            receivedMessage = message;
+            success();
+            return { drop: false };
+          },
+          routes: {
+            "/quotes": {
+              services: {
+                quoteservice: {
+                  type: "http" as "http",
+                  url: "http://localhost:6666/quotes",
+                },
+              },
+            },
+          },
+        },
+      };
+
+      const servers = await startWithConfiguration(
+        undefined,
+        "testinstance",
+        config
+      );
+      app.servers = servers;
+
+      const ws = new WebSocket(
+        `ws://localhost:${
+          (app.servers.httpServer.address() as any).port
+        }/quotes`
+      );
+
+      ws.on("open", () => {
+        ws.send("HELO");
+      });
+    });
+
+    const response = await connectedPromise;
+    ran.should.be.true();
+    receivedMessage.should.equal("HELO");
+  });
+
+  it(`runs the connect hook on route config`, async () => {
+    let ran = false;
+    let receivedMessage = "";
+    let clientResponse = "";
+
+    const connectedPromise: Promise<void> = new Promise(async (success) => {
+      const config = {
+        instanceId: random(),
+        webSocket: {
+          routes: {
+            "/quotes": {
+              onConnect: async (requestId: string, message: string) => {
+                ran = true;
+                receivedMessage = message;
+                success();
+                return { drop: false };
+              },
+              services: {
+                quoteservice: {
+                  type: "http" as "http",
+                  url: "http://localhost:6666/quotes",
+                },
+              },
+            },
+          },
+        },
+      };
+
+      const servers = await startWithConfiguration(
+        undefined,
+        "testinstance",
+        config
+      );
+      app.servers = servers;
+
+      const ws = new WebSocket(
+        `ws://localhost:${
+          (app.servers.httpServer.address() as any).port
+        }/quotes`
+      );
+
+      ws.on("open", () => {
+        ws.send("HELO");
+      });
+    });
+
+    const response = await connectedPromise;
+    ran.should.be.true();
+    receivedMessage.should.equal("HELO");
+  });
+
   it(`runs the connect hook on root config and drops`, async () => {
     let ran = false;
     let receivedMessage = "";
