84d2ba358df9e6b6e2149ba782aecd3f15bc3b27
Jeswin <jeswinpk@agilehead.com>
2020-06-05 07:21:45 +0530

    Move tests to their own directories

---
+++ src/test/integration/connections/websocket/backends/redis/sendToClient.ts
@@ -0,0 +1,142 @@
+// import request = require("supertest");
+// import { doPubSub } from "./utils";
+// import random from "../../../../../../lib/random";
+// import { Server } from "http";
+// import WebSocket from "ws";
+
+// export default async function (app: {
+//   servers: {
+//     httpServer: Server;
+//     websocketServers: WebSocket.Server[];
+//   };
+// }) {
+//   it(`shows generic errors for service`, async () => {
+//     const config = {
+//       instanceId: random(),
+//       http: {
+//         routes: {
+//           "/users": {
+//             POST: {
+//               services: {
+//                 userservice: {
+//                   type: "redis" as "redis",
+//                   requestChannel: "input",
+//                 },
+//                 messagingservice: {
+//                   type: "redis" as "redis",
+//                   requestChannel: "input",
+//                 },
+//               },
+//               genericErrors: true,
+//             },
+//           },
+//         },
+//         redis: {
+//           responseChannel: "output",
+//         },
+//       },
+//     };
+
+//     const serviceResults = [
+//       {
+//         id: "temp",
+//         service: "userservice",
+//         response: {
+//           content: {
+//             user: 1,
+//           },
+//         },
+//       },
+//       {
+//         id: "temp",
+//         service: "messagingservice",
+//         response: {
+//           content: "Hello world",
+//         },
+//       },
+//     ];
+
+//     const result = await doPubSub(
+//       app,
+//       config,
+//       serviceResults,
+//       (success, getJson) => {
+//         request(app.servers.httpServer)
+//           .post("/users")
+//           .send({ hello: "world" })
+//           .set("origin", "http://localhost:3000")
+//           .then((x) => success([x, getJson()]));
+//       }
+//     );
+
+//     const [response, json] = result;
+//     json.request.headers.origin.should.equal("http://localhost:3000");
+//     response.status.should.equal(500);
+//     response.text.should.equal("Internal Server Error.");
+//   });
+
+//   it(`shows generic errors for all services`, async () => {
+//     const config = {
+//       instanceId: random(),
+//       http: {
+//         routes: {
+//           "/users": {
+//             POST: {
+//               services: {
+//                 userservice: {
+//                   type: "redis" as "redis",
+//                   requestChannel: "input",
+//                 },
+//                 messagingservice: {
+//                   type: "redis" as "redis",
+//                   requestChannel: "input",
+//                 },
+//               },
+//             },
+//           },
+//         },
+//         genericErrors: true,
+//         redis: {
+//           responseChannel: "output",
+//         },
+//       },
+//     };
+
+//     const serviceResults = [
+//       {
+//         id: "temp",
+//         service: "userservice",
+//         response: {
+//           content: {
+//             user: 1,
+//           },
+//         },
+//       },
+//       {
+//         id: "temp",
+//         service: "messagingservice",
+//         response: {
+//           content: "Hello world",
+//         },
+//       },
+//     ];
+
+//     const result = await doPubSub(
+//       app,
+//       config,
+//       serviceResults,
+//       (success, getJson) => {
+//         request(app.servers.httpServer)
+//           .post("/users")
+//           .send({ hello: "world" })
+//           .set("origin", "http://localhost:3000")
+//           .then((x) => success([x, getJson()]));
+//       }
+//     );
+
+//     const [response, json] = result;
+//     json.request.headers.origin.should.equal("http://localhost:3000");
+//     response.status.should.equal(500);
+//     response.text.should.equal("Internal Server Error.");
+//   });
+// }
