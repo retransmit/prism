71de9eda4340b8c0fbe34bf0cc7444bca2ac75e9
Jeswin <jeswinpk@agilehead.com>
2020-06-29 09:05:12 +0530

    Add directories for performance tests. No tests yet.

--- src/test/test.ts
+++ src/test/test.ts
@@ -2,8 +2,8 @@ import "mocha";
 import "should";
 import WebSocket from "ws";
 
-import integrationTestsHttp from "./integration/clients/http";
-import integrationTestsWebSocket from "./integration/clients/webSocket";
+import integrationTestsHttp from "./integration/connections/http";
+import integrationTestsWebSocket from "./integration/connections/webSocket";
 
 import { closeHttpServer } from "./utils/http";
 import { closeWebSocketServer } from "./utils/webSocket";
