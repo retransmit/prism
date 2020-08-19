6a4f7ff23e0c21ceef099cd445b270b827613e50
Jeswin <jeswinpk@agilehead.com>
2020-08-19 13:21:34 +0530

    Add test for onRequest hook

---
+++ src/lib/promiseSignal.ts
@@ -0,0 +1,11 @@
+export default function promiseSignal<T>() {
+  let resolve: (
+    value?: T | PromiseLike<T> | undefined
+  ) => void = undefined as any;
+  let reject: (reason?: any) => void = undefined as any;
+  const promise: Promise<T> = new Promise<T>((fnResolve, fnReject) => {
+    resolve = fnResolve;
+    reject = fnReject;
+  });
+  return { promise, resolve, reject };
+}
--- src/test/integration/connections/webSocket/plugins/onRequest.ts
+++ src/test/integration/connections/webSocket/plugins/onRequest.ts
@@ -4,45 +4,49 @@ import startRetransmitTestInstance from "../../../../utils/startRetransmitTestIn
 import { UserAppConfig } from "../../../../../types/config";
 import { TestEnv } from "../../..";
 import { WebSocketClientRequest } from "../../../../../types/webSocket";
+import promiseSignal from "../../../../../lib/promiseSignal";
 
 export default async function (app: TestAppInstance, testEnv: TestEnv) {
   it(`runs the onRequest hook on root config`, async () => {
     let ran = false;
     let receivedMessage = "";
 
-    const connectedPromise: Promise<void> = new Promise(async (success) => {
-      const config: UserAppConfig = {
-        webSocket: {
-          onRequest: async (request: WebSocketClientRequest) => {
-            ran = true;
-            receivedMessage = request.message || "";
-            success();
-            return {
-              handled: true,
-              response: {
-                id: request.id,
-                service: "",
-                type: "message",
-                message: "GOOG:1000.00",
-              },
-            };
-          },
-          routes: {
-            "/quotes": {
-              services: {
-                quoteservice: {
-                  type: "http" as "http",
-                  url: "http://localhost:6666/quotes",
-                },
+    const {
+      promise: onRequestPromise,
+      resolve: onRequestResolve,
+    } = promiseSignal();
+
+    const config: UserAppConfig = {
+      webSocket: {
+        onRequest: async (request: WebSocketClientRequest) => {
+          ran = true;
+          receivedMessage = request.message || "";
+          onRequestResolve();
+          return {
+            handled: true,
+            response: {
+              id: request.id,
+              service: "",
+              type: "message",
+              message: "GOOG:1000.00",
+            },
+          };
+        },
+        routes: {
+          "/quotes": {
+            services: {
+              quoteservice: {
+                type: "http" as "http",
+                url: "http://localhost:6666/quotes",
               },
             },
           },
         },
-      };
+      },
+    };
 
-      const appControl = await startRetransmitTestInstance({ config });
-      app.appControl = appControl;
-    });
+    const appControl = await startRetransmitTestInstance({ config });
+    app.appControl = appControl;
 
     const ws = new WebSocket(`ws://localhost:${app.appControl?.port}/quotes`);
 
@@ -50,61 +54,84 @@ export default async function (app: TestAppInstance, testEnv: TestEnv) {
       ws.send("HELO");
     });
 
-    const receivedResponsePromise = new Promise<string>((success) => {
-      // Listen for messages
-      ws.addEventListener("message", function (event) {
-        success(event.data);
-      });
+    const {
+      promise: onDataReceivedPromise,
+      resolve: onDataReceivedResolve,
+    } = promiseSignal<string>();
+
+    ws.addEventListener("message", function (event) {
+      onDataReceivedResolve(event.data);
     });
 
-    await connectedPromise;
+    await onRequestPromise;
     ran.should.be.true();
     receivedMessage.should.equal("HELO");
-    
-    const response = await receivedResponsePromise;
-    response.should.equal("BLALSA");
+
+    const response = await onDataReceivedPromise;
+    response.should.equal("GOOG:1000.00");
   });
 
-  // it(`runs the connect hook on route config`, async () => {
-  //   let ran = false;
-  //   let receivedMessage = "";
+  it(`runs the onRequest hook on route config`, async () => {
+    let ran = false;
+    let receivedMessage = "";
 
-  //   const connectedPromise: Promise<void> = new Promise(async (success) => {
-  //     const config: UserAppConfig = {
-  //       webSocket: {
-  //         routes: {
-  //           "/quotes": {
-  //             onConnect: async (request: WebSocketClientRequest) => {
-  //               ran = true;
-  //               receivedMessage = request.message || "";
-  //               success();
-  //               return { drop: false };
-  //             },
-  //             services: {
-  //               quoteservice: {
-  //                 type: "http" as "http",
-  //                 url: "http://localhost:6666/quotes",
-  //               },
-  //             },
-  //           },
-  //         },
-  //       },
-  //     };
+    const {
+      promise: onRequestPromise,
+      resolve: onRequestResolve,
+    } = promiseSignal();
 
-  //     const appControl = await startRetransmitTestInstance({ config });
+    const config: UserAppConfig = {
+      webSocket: {
+        routes: {
+          "/quotes": {
+            onRequest: async (request: WebSocketClientRequest) => {
+              ran = true;
+              receivedMessage = request.message || "";
+              onRequestResolve();
+              return {
+                handled: true,
+                response: {
+                  id: request.id,
+                  service: "",
+                  type: "message",
+                  message: "GOOG:1000.00",
+                },
+              };
+            },
+            services: {
+              quoteservice: {
+                type: "http" as "http",
+                url: "http://localhost:6666/quotes",
+              },
+            },
+          },
+        },
+      },
+    };
 
-  //     app.appControl = appControl;
-  //     const { port } = appControl;
+    const appControl = await startRetransmitTestInstance({ config });
+    app.appControl = appControl;
 
-  //     const ws = new WebSocket(`ws://localhost:${port}/quotes`);
+    const ws = new WebSocket(`ws://localhost:${app.appControl?.port}/quotes`);
 
-  //     ws.on("open", () => {
-  //       ws.send("HELO");
-  //     });
-  //   });
+    ws.on("open", () => {
+      ws.send("HELO");
+    });
 
-  //   const response = await connectedPromise;
-  //   ran.should.be.true();
-  //   receivedMessage.should.equal("HELO");
-  // });
+    const {
+      promise: onDataReceivedPromise,
+      resolve: onDataReceivedResolve,
+    } = promiseSignal<string>();
+
+    ws.addEventListener("message", function (event) {
+      onDataReceivedResolve(event.data);
+    });
+
+    await onRequestPromise;
+    ran.should.be.true();
+    receivedMessage.should.equal("HELO");
+
+    const response = await onDataReceivedPromise;
+    response.should.equal("GOOG:1000.00");
+  });
 }
